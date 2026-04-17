7d80bc0d55d1298fe3e22bc7200f045508fc4646
sun <sunny@sunnymorgan.com>
2026-04-17 13:48:05 -0400

    * Flashblocks Module: yes

--- flashblocks-truncate-content.php
+++ flashblocks-truncate-content.php
@@ -10,6 +10,9 @@
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain:       flashblocks-truncate-content
+ * Flashblocks Module: yes
+ * Flashblocks Category: content
+ * Flashblocks Tags: truncate, read-more, collapse
  *
  * @package FlashblocksTruncateContent
  */
